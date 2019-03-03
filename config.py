9893efb3dcd05dba99079d8aadfcae27f605db71
Clark Chen <clarkczj@hotmail.com>
2019-03-02 17:44:40 -0800

    update config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import filetype
 # API Access Module
-
 # filter information
 # txt, pdf, doc, docx, xls, ppt, pptx
 def filter(true):
@@ -32,7 +31,6 @@ file_dir = input("please put a directory that you want to store the backup")
 write_time = input("please put a time that you want to back up the file")
 # and when to delete them
 delete_time = input("please put a time that you want to delete the previous backup file")
-
 
 if __name__ == '__main__':
 	#Config Google Account and password
