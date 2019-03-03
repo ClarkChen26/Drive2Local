3321f15b25788a0653d12b8557860eac208d8401
Clark Chen <clarkczj@hotmail.com>
2019-03-02 16:49:40 -0800

    basics

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 # API Access Module
-
 # filter information
+filetype = ''
 # login credentials 
+USERNAME = ''
+PASSWORD = ''
 
 # File Handling Module
-
 # where to write them
+file_dir = ''
 # how often to write them
+write_time = ''
 # and when to delete them
+delete_time = ''
