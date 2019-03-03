00b87187d5987bebf314f6a5a9759e02b635e731
Clark Chen <clarkczj@hotmail.com>
2019-03-02 16:57:44 -0800

    extent list

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # API Access Module
 # filter information
+# txt, pdf, doc, docx, xls, ppt, pptx
 filetype = ''
 # login credentials 
 USERNAME = input("please input a google account: ")
 PASSWORD = input("please input password: ")
-
 # File Handling Module
 # where to write them
 file_dir = ''
 # how often to write them
-write_time = ''
+write_time = input()
 # and when to delete them
 delete_time = ''
