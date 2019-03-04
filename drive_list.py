5ed8b01eec5c8aa6814169cc76f3cebd59adc7a9
Joseph Shea-Bianco <j.sheabianco@gmail.com>
2019-03-03 21:00:41 -0800

    updated drive_list, removed swap file, removed storage

--- drive_list.py
+++ drive_list.py
@@ -4,14 +4,28 @@ from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
-store = file.Storage('storage.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+def getDrive():
+	# Define the permission scope
+	SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
+	# Define the local credential store
+	store = file.Storage('storage.json')
+	# Get credentials from the local store
+	creds = store.get()
+	# If the credentials are invalid, get new credentials from the user
+	if not creds or creds.invalid:
+	    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+	    creds = tools.run_flow(flow, store)
 
-files = DRIVE.files().list().execute().get('files', [])
-for f in files:
-    print(f['name'], f['mimeType'])
+	# Get the Google Drive of the credentialed user
+	DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+
+	return DRIVE
+
+def listFiles(DRIVE):
+	files = DRIVE.files().list().execute().get('files', [])
+	for f in files:
+	    print(f['name'], f['mimeType'])
+
+if __name__ == '__main__':
+	DRIVE = getDrive()
+	listFiles(DRIVE)
