6bab8861532ed622187524c1cbbec5b32ed39304
Joseph Shea-Bianco <j.sheabianco@gmail.com>
2019-03-07 19:31:30 -0800

    Fixed one of the dependencies in the install_deps script

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip3 install apiclient google-api-python-client google-auth-httplib2 google-auth-oauthlib oauthclient2 python-crontab
+pip3 install apiclient google-api-python-client google-auth-httplib2 google-auth-oauthlib oauth2client python-crontab
